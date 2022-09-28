76e0e725fc08f4379975590595c75a2fd03c5198
SrikanthEmbadi <srikanthembadi10@gmail.com>
2022-09-28 12:09:20 +0530

    first commit

---
+++ vdom/src/App.js
@@ -0,0 +1,19 @@
+import ReactDOM from 'react-dom';
+import './App.css';
+
+function App() 
+{
+ const element= 
+ (
+    <div >
+      <h1>Welcome to CBIT</h1>
+      <h1>Now the Time is</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+  setInterval(App, 1000);
+}
+
+
+export default App;
